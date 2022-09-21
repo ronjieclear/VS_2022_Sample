6867ca50ca8ad9b59c3cb9c02f96cb7525b091e2
RONJIE <RONJIE@HP>
2022-09-21 15:43:20 +0800

    Add project files.

---
+++ WindowsFormsApp18/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hi Hello there ROnjie Malinao");
+        }
+    }
+}
